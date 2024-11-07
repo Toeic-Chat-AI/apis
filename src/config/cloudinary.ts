3934f159cf51d9fbd7d4c004a318ab5cfa9d7a2c
Lithesys <104113556+Lithesys@users.noreply.github.com>
2024-11-07 12:30:56 +0700

    test: add cloudinary

---
+++ src/config/cloudinary.ts
@@ -0,0 +1,10 @@
+import { v2 as cloudinary } from "cloudinary";
+import envConfig from "./dotenv";
+
+cloudinary.config({
+  cloud_name: envConfig.CLOUDINARY_NAME,
+  api_key: envConfig.CLOUDINARY_API_KEY,
+  api_secret: envConfig.CLOUDINARY_SECRET
+});
+
+export default cloudinary;
